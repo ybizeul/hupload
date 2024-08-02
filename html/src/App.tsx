69227080a71345379f1dae73de71a4da72dcb829
Yann Bizeul <yann@tynsoe.org>
2024-08-02 09:20:19 +0200

    new: refine UI for back and logout interface elements

--- html/src/App.tsx
+++ html/src/App.tsx
@@ -2,15 +2,15 @@ import "@mantine/core/styles.css";
 import '@mantine/dropzone/styles.css';
 
 import { useEffect, useState } from "react";
-import { ActionIcon, Affix, Container, MantineProvider, Menu } from "@mantine/core";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Container, MantineProvider } from "@mantine/core";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { H } from "./APIClient";
 import { Share, Login, Shares } from "@/Pages";
 
-import { IconMenu2 } from "@tabler/icons-react";
 import { LoggedInContext } from "@/LoggedInContext";
 import { VersionComponent } from "@/Components";
+import { Haffix } from "./Components/Haffix";
 
 // Logged in user is passed to the context
 interface LoggedIn {
@@ -20,8 +20,6 @@ interface LoggedIn {
 export default function App() {
   // Component state
   const [loggedIn, setLoggedIn ] = useState<string|null>(null)
-  
-  // Component Hooks
 
   // Check with server current logged in state
   // This is typically executed once when Hupload is loaded
@@ -35,36 +33,17 @@ export default function App() {
       setLoggedIn(null)
       console.log(e)
     })
-  })
+  },[])
 
   return (
   <MantineProvider defaultColorScheme='auto'>
     <Container mt="md" h="100%">
       <BrowserRouter>
         <LoggedInContext.Provider value={{loggedIn,setLoggedIn}}>
-        {loggedIn &&
-          <Affix position={{ bottom: 20, right: 20 }}>
-            <Menu trigger="hover" openDelay={100} closeDelay={400} shadow="md" width={200}>
-              <Menu.Target>
-                <ActionIcon variant="filled" size="xl" radius="xl" aria-label="Settings">
-                  <IconMenu2 style={{ width: '70%', height: '70%' }} stroke={1.5} />
-                </ActionIcon>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Item component={Link} to="/shares">
-                  Shares
-                </Menu.Item>
-                <Menu.Item onClick={() => { H.logoutNow(); window.location.href='/'}}>
-                Logout
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
-          </Affix>
-          }
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path=":share" element={<Share />} />
-            <Route path="/shares" element={<Shares owner={loggedIn}/>} />
+        <Routes>
+            <Route path="/" element={<><Haffix/><Login /></>}/>
+            <Route path="/shares" element={<><Haffix/><Shares owner={loggedIn}/></>} />
+            <Route path=":share" element={<><Haffix/><Share /></>} />
           </Routes>
         </LoggedInContext.Provider>
       </BrowserRouter>
---
+++ html/src/Components/Haffix.tsx
@@ -0,0 +1,24 @@
+import { ActionIcon, Affix, Tooltip } from "@mantine/core";
+import { IconArrowLeft, IconLogout } from "@tabler/icons-react";
+import { Link, useMatch } from "react-router-dom";
+import { H } from "@/APIClient";
+
+export function Haffix() {
+    const location = useMatch('/:share')
+    return(
+        <>
+            <Affix position={{ bottom: 20, left: 20 }}>
+            {location?.params.share != 'shares' &&
+                <Tooltip withArrow arrowOffset={10} arrowSize={4} label="Back">
+                    <ActionIcon size="lg" component={Link} to="/shares" color="blue" radius="xl"><IconArrowLeft style={{ width: '70%', height: '70%' }} /></ActionIcon>
+                </Tooltip>
+                }
+            </Affix>
+            <Affix position={{ bottom: 20, right: 20 }}>
+                <Tooltip withArrow arrowOffset={10} arrowSize={4} label="Logout">
+                    <ActionIcon size="lg" variant="light" color="red" radius="xl" onClick={() => { H.logoutNow(); window.location.href='/'}}><IconLogout style={{ width: '70%', height: '70%' }} /></ActionIcon>
+                </Tooltip>
+            </Affix>
+        </>
+    )
+}
