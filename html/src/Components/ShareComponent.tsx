032e722e6eeb8d85658df97d99b08d1af94f0514
Yann Bizeul <yann@tynsoe.org>
2024-08-02 11:38:14 +0200

    new: Add "Copy" button for share URLs

--- html/src/Components/ShareComponent.tsx
+++ html/src/Components/ShareComponent.tsx
@@ -1,8 +1,8 @@
-import { ActionIcon, Anchor, Button, Group, Paper, Popover, Text } from "@mantine/core";
+import { ActionIcon, Anchor, Button, CopyButton, Group, Paper, Popover, Text, Tooltip } from "@mantine/core";
 import { Share } from "../hupload";
 import { Link } from "react-router-dom";
 import classes from './ShareComponent.module.css';
-import { IconTrash } from "@tabler/icons-react";
+import { IconClipboardCopy, IconTrash } from "@tabler/icons-react";
 import { useState } from "react";
 import { H } from "@/APIClient";
 
@@ -29,17 +29,30 @@ export function ShareComponent(props: {share: Share}) {
         <Paper key={key} p="md" shadow="xs" radius="md" mt={10} className={classes.paper}>
             <Group justify="space-between" h={45}>
                 <Anchor component={Link} to={'/'+name}><Text>{name}</Text></Anchor>
-                <Popover width={200} position="bottom" withArrow shadow="md">
-                    <Popover.Target>
-                        <ActionIcon variant="light" color="red" >
-                            <IconTrash style={{ width: '70%', height: '70%' }} stroke={1.5}/>
-                        </ActionIcon>
-                    </Popover.Target>
-                    <Popover.Dropdown className={classes.popover}>
-                        <Text ta="center" size="xs" mb="xs">Delete this share ?</Text>
-                        <Button aria-description="delete" w="100%" variant='default' c='red' size="xs" onClick={deleteShare}>Delete</Button>
-                    </Popover.Dropdown>
-                </Popover>
+                <Group>
+                    <CopyButton value={window.location.protocol + '//' + window.location.host + '/' + name}>
+                    {({ copied, copy }) => (
+                        <Tooltip withArrow arrowOffset={10} arrowSize={4} label="Copy URL">
+                            <ActionIcon variant="light" color={copied ? 'teal' : 'blue'} onClick={copy} >
+                            <IconClipboardCopy style={{ width: '70%', height: '70%' }} stroke={1.5}/>
+                            </ActionIcon>
+                        </Tooltip>
+                    )}
+                    </CopyButton>
+                    <Popover width={200} position="bottom" withArrow shadow="md">
+                        <Popover.Target>
+                            <Tooltip withArrow arrowOffset={10} arrowSize={4} label="Delete Share">
+                                <ActionIcon variant="light" color="red" >
+                                    <IconTrash style={{ width: '70%', height: '70%' }} stroke={1.5}/>
+                                </ActionIcon>
+                            </Tooltip>
+                        </Popover.Target>
+                        <Popover.Dropdown className={classes.popover}>
+                            <Text ta="center" size="xs" mb="xs">Delete this share ?</Text>
+                            <Button aria-description="delete" w="100%" variant='default' c='red' size="xs" onClick={deleteShare}>Delete</Button>
+                        </Popover.Dropdown>
+                    </Popover>
+                </Group>
             </Group>
         </Paper>
         }
