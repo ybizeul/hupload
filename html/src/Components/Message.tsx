526b7a8e667b7cb87d6e2905f6b8d8d62ceee043
Yann Bizeul <yann@tynsoe.org>
2024-08-24 08:28:41 +0200

    Fix text overflow in code paragraphs

--- html/src/Components/Message.tsx
+++ html/src/Components/Message.tsx
@@ -10,7 +10,7 @@ export function Message(props: MessageProps&BoxComponentProps){
     const { value } = props;
     return (
         <Paper flex="1" withBorder pt="5.5" px="12" display="flex" {...props}>
-            <TypographyStylesProvider flex="1" fs="sm">
+            <TypographyStylesProvider flex="1" fs="sm" maw="100%">
                 <div className="message" dangerouslySetInnerHTML={{ __html: marked.parse(value)}}></div>
             </TypographyStylesProvider>
         </Paper>
