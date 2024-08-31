a216e350de92e58041a18fa862bfba0999e0b6ba
Yann Bizeul <yann@tynsoe.org>
2024-08-31 11:01:34 +0200

    upload directly from shares page

--- html/src/Components/ShareComponent.tsx
+++ html/src/Components/ShareComponent.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, ActionIconGroup, Anchor, Box, Button, CopyButton, Flex, Group, Paper, Popover, Stack, Text, Tooltip, useMantineTheme } from "@mantine/core";
+import { ActionIcon, ActionIconGroup, Anchor, Box, Button, CopyButton, Flex, Group, Paper, Popover, Progress, Stack, Text, Tooltip, useMantineTheme } from "@mantine/core";
 import { humanFileSize, prettyfiedCount, Share } from "../hupload";
 import { Link } from "react-router-dom";
 import classes from './ShareComponent.module.css';
@@ -16,6 +16,9 @@ export function ShareComponent(props: {share: Share}) {
     const [share,setShare] = useState(props.share)
     const [deleted,setDeleted] = useState(false)
     const [newOptions, setNewOptions] = useState<Share["options"]>(share.options)
+    const [uploadPercent, setUploadPercent] = useState(0)
+    const [uploading, setUploading] = useState(false)
+
     const theme = useMantineTheme();
     const isBrowser = useMediaQuery('(min-width: +' + theme.breakpoints.xs + ')');
 
@@ -47,29 +50,29 @@ export function ShareComponent(props: {share: Share}) {
     return (
         <>
         <Paper key={key} withBorder shadow="xs" radius="md" mt={10} pos="relative" className={classes.paper}>
-            <Dropzone p={0} m={0} activateOnClick={false} enablePointerEvents={true} w={"100%"} h={"100%"}
+            <Dropzone p={0} m={0} activateOnClick={false} enablePointerEvents={true} w={"100%"} h={"100%"} style={{border:"none", backgroundColor:"transparent"}}
                 onDrop={(files) => {
                     const updateProgress = (progress: QueueItem[]) => {
+                        const loaded = progress.reduce((acc, item) => acc + item.loaded, 0)
+                        const total = progress.reduce((acc, item) => acc + item.total, 0)
+                        setUploadPercent(loaded/total*100)
                         console.log(progress)
                     }
                     const queue = new UploadQueue(H,"/shares/"+name, updateProgress)
-
+                    setUploading(true)
                     queue.addFiles(files)
-                        // .then((r) => {
-                            
-                        //     //const finishedItems = r as Item[]
-
-                        //     //setQueueItems([])
-                        //     setItems([...finishedItems, ...newItems])
-                        // })
+                        .then(() => {
+                            setUploading(false)
+                        })
                         .catch((e) => {
+                            setUploading(false)
                             console.log(e)
                         })
                 }}
 
                 onReject={(files) => console.log('rejected files', files)}
                 >
-                <Group justify="center" gap="xl" mih={100} style={{ pointerEvents: 'none' }}>
+                    {/* Share component footer */}
                     <Group wrap="nowrap" flex={1} w="100%" pos="absolute" bottom="0.1em" style={{justifyContent:"center"}} align="center" gap="0.2em">
                         <IconClock color={(remaining===null || remaining > 0 )?"gray":"red"} size="0.8em"  width={"1em"}/>
                         <Text style={{ whiteSpace: "nowrap"}} size="xs" c="gray">{
@@ -82,7 +85,9 @@ export function ShareComponent(props: {share: Share}) {
                             prettyfiedCount(remaining,"day","days",null) + " left"} | {share.options.exposure==="download"?"Guests can download":(share.options.exposure==="both"?"Guests can upload & download":"Guests can upload")}
                         </Text>
                     </Group>
-
+                    {(uploading || uploadPercent > 0) &&
+                            <Progress pos="absolute" bottom="0" w="100%" size="xs" color="green" value={uploadPercent} />
+                        }
                     {/* Share informations */}
                     <Box p="lg">
                         <Stack gap="0">
@@ -137,7 +142,6 @@ export function ShareComponent(props: {share: Share}) {
                             </Flex>
                             {share.options.description && <Text w="100%" size="xs" c="gray">{share.options.description}</Text>}</Stack>
                     </Box>
-                </Group>
                 </Dropzone>
             {/* Share component footer */}
         </Paper>
