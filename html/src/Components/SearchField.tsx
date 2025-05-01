ea319891acae745ef0338983388a2b68946a38d6
Yann Bizeul <yann@tynsoe.org>
2025-05-01 12:07:22 +0200

    new: search field

---
+++ html/src/Components/SearchField.tsx
@@ -0,0 +1,20 @@
+import { ActionIcon, TextInput } from '@mantine/core';
+import { IconSearch, IconX } from '@tabler/icons-react';
+
+export function SearchField(props: { value: string, onChange?: (e: string) => void }) {
+    const { value, onChange } = props;
+    
+    return (
+      <TextInput
+        leftSection={<IconSearch size={16} stroke={1.5} />}
+        rightSection={<ActionIcon size={16} radius="xl" variant="subtle" color="gray" onClick={() => {onChange && onChange("")}}><IconX size={16} stroke={1.5} /></ActionIcon>}
+        placeholder="Search"
+        value={value}
+        radius={30}
+        w="50%"
+        onChange={(event) => {
+          onChange && onChange(event.currentTarget.value);
+        }}
+      />
+    );
+  }
