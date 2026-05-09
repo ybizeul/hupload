84bb4f3f6b738743997eb68816b5a0e805c976bc
Yann Bizeul <yann@tynsoe.org>
2026-05-09 09:13:01 +0200

    fix: add LogoutPage component and update routing in App (closes #69)

--- html/src/App.tsx
+++ html/src/App.tsx
@@ -3,6 +3,7 @@ import '@mantine/dropzone/styles.css';
 import "./i18n/config.ts";
 
 import { Container, MantineProvider } from "@mantine/core";
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { SharePage, Login, SharesPage } from "@/Pages";
@@ -10,6 +11,7 @@ import { SharePage, Login, SharesPage } from "@/Pages";
 import { AuthContext, useAuthInfo } from "@/AuthContext";
 import { VersionComponent, Haffix } from "@/Components";
 import { ErrorPage } from "./Pages/ErrorPage.tsx";
+import { H } from "./APIClient.ts";
 
 
 
@@ -23,6 +25,7 @@ export default function App() {
             <AuthContext.Provider value={{authInfo,check}}>
                 <Routes>
                     <Route path="/" element={<Login />}/>
+                    <Route path="/logout" element={<LogoutPage check={check} />}/>
                     <Route path="/error" element={<ErrorPage/>}/>
                     <Route path="/shares" element={<>{authInfo?.user&&<Haffix/>}<SharesPage owner={authInfo?.user?(authInfo?.user):null}/></>} />
                     <Route path=":share" element={<>{authInfo?.user&&<Haffix/>}<SharePage /></>} />
@@ -32,4 +35,17 @@ export default function App() {
         </Container>
         {authInfo?.user&&<VersionComponent/>}
     </MantineProvider>)
+}
+
+function LogoutPage(props: { check?: () => void }) {
+    const { check } = props
+
+    useEffect(() => {
+        document.cookie = "hupload=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+        H.logoutNow()
+        check && check()
+        window.location.replace("/")
+    }, [check])
+
+    return null
 }
